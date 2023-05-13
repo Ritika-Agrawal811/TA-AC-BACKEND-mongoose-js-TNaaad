8378b9c66185ddf655a93520a69d3d3e347d8df4
Ritika Agrawal <ritikaagrawal339@gmail.com>
2023-05-13 19:42:06 +0530

    complete: Assignment Models

---
+++ block-BNaacm/project/src/database.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const server = "127.0.0.1:27017";
+const database = "sample";
+
+class Database {
+  constructor() {
+    this._connect();
+  }
+
+  _connect() {
+    mongoose
+      .connect(`mongodb://${server}/${database}`)
+      .then(() => {
+        console.log("Database connected sucessfully!");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+}
+
+module.exports = new Database();
